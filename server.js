b530fe82936b298fac9439fe4853f2d96f30c22e
Allan Wazacz <cazwazacz@gmail.com>
2017-01-30 19:43:55 +0000

    second commit

--- server.js
+++ server.js
@@ -2,25 +2,55 @@ var mongodb = require('mongodb')
 var express = require('express')
 var app = express()
 const ImagesClient = require('google-images');
-var cseid = '005930505710033046788:c2wonj71wqw';
-var apikey = 'AIzaSyCE4354_Pid-jGvJaKgaWKItp9vAB9CFg8';
+var cseid = process.env.CSE_ID;
+var url = process.env.MONGOLAB_URI;
+var apikey = process.env.API_KEY;
 let client = new ImagesClient(cseid, apikey);
 
 app.use(express.static('public'));
 var MongoClient = mongodb.MongoClient;
 
-app.get('/:searchQuery', function(req, res){
-  var searchQuery = req.params.searchQuery
-  console.log(searchQuery)
-  client.search(searchQuery)
-      .then(function (images) {
-        res.json(images);
-      });
-})
+MongoClient.connect(url, function(err,db){
+  if (err){
+    console.log(err)
+  } else {
+    console.log('connection established to ' + url)
+    var history = db.collection('history')
 
 
-var port = process.env.PORT || 3000;
+    app.get('/api/imagesearch/:searchQuery', function(req, res){
+      var searchQuery = req.params.searchQuery
+      var offset = req.query.offset
+      console.log(searchQuery)
+      console.log(offset)
+      client.search(searchQuery, {page: offset})
+          .then(function (images) {
+            res.json(images);
+          });
+      history.insert({term: searchQuery, when: new Date()}, function(err, result){
+        if (err){
+          console.log('inserting error ' + err)
+        }
+      })
+    })
+    app.get('/api/latest/imagesearch/', function(req, res){
+      var cursor = history.find({}, {_id: 0}).sort({_id:-1}).limit(10);
+      cursor.toArray(function(err, result){
+        if (err){
+          console.log('find error ' + err)
+        } else if (result.length) {
+          res.json(result);
+        }
+      })
+    })
 
-app.listen(port, function(){
-  console.log('App listening at ' + port)
-})
+
+
+    var port = process.env.PORT || 3000;
+
+    app.listen(port, function(){
+      console.log('App listening at ' + port)
+    })
+  }
+
+});
